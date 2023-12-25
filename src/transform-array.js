c7e47518f5b8e68af61f6ee3d3f379bbec729f61
makzhentos <makzhentos@gmail.com>
2023-12-25 23:03:34 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (Array.isArray(arr)) {
+    let transform = arr.slice();
+    let sqc = ['--double-next', '--double-prev', '--discard-next', '--discard-prev']
+    // цикл
+    for (i = 0; i < transform.length; i++) {
+      if (transform[i] === '--double-next' && i !== transform.length - 1) {
+        if (!sqc.includes(transform[i + 1])) {
+          transform.splice(i + 1, 0, transform[i + 1]); 
+        }
+      } else if (transform[i] === '--double-prev' && i !== 0) {
+        if (!sqc.includes(transform[i - 1])) {
+          transform.splice(i + 1, 0, transform[i - 1]); 
+        }      
+      } else if (transform[i] === '--discard-next' && i !== transform.length - 1) {
+        if (!sqc.includes(transform[i + 1])) {
+          transform.splice(i + 1, 1); 
+        }
+      } else if (transform[i] === '--discard-prev' && i !== 0) {
+        if (!sqc.includes(transform[i - 1])) {
+          transform.splice(i - 1, 1); 
+        }
+      }
+    }
+
+    // 'arr' parameter must be an instance of the Array!
+
+    let sum = [];
+    transform.forEach((item) => {
+      if (!sqc.includes(item)) {
+        sum.push(item)
+      }
+    })
+    return sum
+  } else {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  
 }
 
 module.exports = {
